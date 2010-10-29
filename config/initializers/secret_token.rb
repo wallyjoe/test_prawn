ef951539e350db2602da20688ef4a6a7f9dfe96a
wally <wallyjoe8413@gmail.com>
2010-10-28 21:23:10 -0400

    first commit try prawn

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TryPrawn::Application.config.secret_token = '8b113999e429236db2f220cc6c28f67dad449c443924b917e1d446e0d65c702f0ee5fa6060c5a322cd0cac7380a86e9f39da05fb3f0a30ff9e960c3aeea456c7'
